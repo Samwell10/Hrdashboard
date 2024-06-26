b243055725b29f235a2d4af17b467d6220cc5296
Precious1324 <aleriwaprecious70@gmail.com>
2024-06-26 23:00:28 +0100

    Final Commit

---
+++ src/components/Filter/CustomFilter.js
@@ -0,0 +1,14 @@
+const CustomFilter = () => {
+    return (  
+        <div className="customfilter">
+            <div className="by-date">
+                <p className="by-date-text">By Date</p>
+                <div className="date-periods">
+                    
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+export default CustomFilter;
